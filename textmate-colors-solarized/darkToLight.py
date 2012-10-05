4c5eebb20d642ecccc783273df2bce33372092e6
Nicholas Moe <ntmoe@csbsju.edu>
2012-10-04 20:04:18 -0500

    Made change to name of dark theme and added light theme

---
+++ textmate-colors-solarized/darkToLight.py
@@ -0,0 +1,22 @@
+f = open('Solarized (Dark)-modified.tmTheme', 'r')
+g = open('Solarized (light)-modified.tmTheme', 'w')
+
+replaceDict = {'#002B36': '#FDF6E3',
+'#073642': '#EEE8D5',
+'#586E75': '#93A1A1',
+'#657B83': '#839496',
+'#839496': '#657B83',
+'#93A1A1': '#586E75',
+'#EEE8D5': '#073642',
+'#FDF6E3': '#002B36'}
+
+for line in f:
+    for k,v in replaceDict.iteritems():
+        if k in line:
+            line = line.replace(k,v)
+            break
+    g.write(line)
+
+f.close()
+g.close()
+
